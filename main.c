e5fc4c6d2e8909d99c350e7674e167a8ba41df9b
nkbrooks <71451988+nkbrooks@users.noreply.github.com>
2020-10-16 00:00:27 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-#include "fill-array.h"
+#include "complete-array.h"
 #include "compare-arrays.h"
-
-/* (c) Larry Herman, 2020.  You are allowed to use this code yourself, but
- * not to provide it to anyone else.
- */
 
 int main(void) {
   int arr1[10];
   int arr2[10]= {216, 216, 216, 216, 216, 216, 216, 216, 216, 216};
 
-  fill_array(arr1, 0, 3, 250);
+  complete_array(arr1, 0, 3, 250);
   printf("%d\n", compare_arrays(arr1, arr2, 10));
 
-  fill_array(arr1, 0, 9, 216);
+  complete_array(arr1, 0, 9, 216);
   printf("%d\n", compare_arrays(arr1, arr2, 10));
 
   return 0;
