600025832c06447e1514cd7bd8e8572847250afe
nkbrooks <71451988+nkbrooks@users.noreply.github.com>
2020-10-15 23:58:11 -0400

    Update and rename fill-array.c to complete-array.c

--- complete-array.c
+++ complete-array.c
@@ -1,9 +1,5 @@
 #include "fill-array.h"
 
-/* (c) Larry Herman, 2020.  You are allowed to use this code yourself, but
- * not to provide it to anyone else.
- */
-
 void fill_array(int arr[], int from, int to, int value) {
   int i;
 
