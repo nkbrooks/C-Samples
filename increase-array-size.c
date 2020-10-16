3474f27d4244007b26516fe8d1a12fac9b16a6b9
nkbrooks <71451988+nkbrooks@users.noreply.github.com>
2020-10-16 00:05:03 -0400

    Update increase-array-size.c

--- increase-array-size.c
+++ increase-array-size.c
@@ -1,7 +1,7 @@
-#include "incr-array.h"
+#include "increase_array_size.h"
 
 
-void incr_array(int a[], int size) {
+void increase_array_size(int a[], int size) {
   size--;
   while (size >= 0) {
     a[size]++;
