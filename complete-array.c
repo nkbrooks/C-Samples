ce22855ffe1e218f0af95ea575635419ae1b2a9b
nkbrooks <71451988+nkbrooks@users.noreply.github.com>
2020-10-16 00:03:26 -0400

    Update complete-array.c

--- complete-array.c
+++ complete-array.c
@@ -1,6 +1,6 @@
 #include "fill-array.h"
 
-void fill_array(int arr[], int from, int to, int value) {
+void complete_array(int arr[], int from, int to, int value) {
   int i;
 
   for (i= from; i <= to; i++)
